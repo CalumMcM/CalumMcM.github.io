919f16663440a230861fc78ac76b171401736c06
Calum McMeekin <calummcmeekin@me.com>
2019-06-21 17:09:40 +0100

    Javascript

---
+++ main.js
@@ -0,0 +1,24 @@
+function myMain(){
+    myFunction();
+}
+
+function myFunction(){
+    var x = document.createElement("BUTTON");
+    document.body.appendChild(x);
+    /*
+    // 1. Create the button
+    const button = document.createElement("button");
+    button.innerHTML = "Get Results!";
+
+    // 2. Append somewhere
+    const body = document.getElementsByTagName("body")[0];
+    body.appendChild(button);
+
+    // 3. Add event handler
+    button.addEventListener ("click", function() {
+    //MY NEW FUNCTION HERE
+        alert("did something");
+    });
+    */
+}
+
