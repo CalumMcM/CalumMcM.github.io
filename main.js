b85fece6f0957d1cdced99cfa5d609ba07f4ade0
Calum McM <calummcmeekin@me.com>
2019-06-26 08:33:42 +0100

    index.html now calls PackWithoutLack

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
-/*
+
+builder 
+
+getter 
+
+setter 
+
+
 function callPackWithoutLackAPI(location, days, hours){
-    
+    // Instantiate the Shell object and invoke its execute method.
+    const execSync = require('child_process').execSync;
+    // import { execSync } from 'child_process'; 
+    call = 'python PackWithoutLack.py ' + location + ' ' +  days + ' ' + hours
+    const output = execSync(call, { encoding: 'utf-8' });  // the default is 'buffer'
+    console.log(output);
 }
-var exec = require('child_process').exec, child;
-*/
-// Instantiate the Shell object and invoke its execute method.
-const execSync = require('child_process').execSync;
-// import { execSync } from 'child_process';  // replace ^ if using ES modules
-call = 'python PackWithoutLack.py' + ' EH39JN ' + '5 ' + '40'
-const output = execSync(call, { encoding: 'utf-8' });  // the default is 'buffer'
-console.log(output);
+callPackWithoutLackAPI('EH39JN', '2', '4')
