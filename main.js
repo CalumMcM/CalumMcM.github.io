b6b53eb7c8431de265e03b2fdb96ea5395751bfe
Calum McMeekin <calummcmeekin@me.com>
2019-06-26 15:44:23 +0100

    Added style for results page

--- main.js
+++ main.js
@@ -1,17 +1,43 @@
-
-builder 
-
-getter 
-
-setter 
-
-
-function callPackWithoutLackAPI(location, days, hours){
-    // Instantiate the Shell object and invoke its execute method.
-    const execSync = require('child_process').execSync;
-    // import { execSync } from 'child_process'; 
-    call = 'python PackWithoutLack.py ' + location + ' ' +  days + ' ' + hours
-    const output = execSync(call, { encoding: 'utf-8' });  // the default is 'buffer'
-    console.log(output);
+class APICall{
+    constructor(location, days, hours, output){
+        this._location = location;
+        this._days = days;
+        this._hours = hours;
+        this._output = output;
+    }
+    get location(){
+        return this._location;
+    }
+    get days(){
+        return this._days;
+    }
+    get hours(){
+        return this._hours;
+    }
+    get output(){
+        return this._output;
+    }
+    set location(newLocation){
+        this._location = newLocation;
+    }
+    set days(newDays){
+        this._days = newDays;
+    }
+    set hours(newHours){
+        this._hours = newHours;
+    }
+    callPackWithoutLackAPI(){
+        // Instantiate the Shell object and invoke its execute method.
+        const execSync = require('child_process').execSync;
+        call = 'python PackWithoutLack.py ' + this._location + ' ' +  this._days + ' ' + this._hours
+        const output = execSync(call, { encoding: 'utf-8' }); 
+        this._output = output; //Setter for output
+    }
+    deconstructOutput(){
+        const BoulderJudgement = this._output['BoulderJudgement'];
+        //Colour Tags: G OKGREEN, L OKLIGHTBLUE, B BLUE, W WARNING, F FAIL, R REDUNDERBLACK
+        let colour = BoulderJudgement.match(/[GLBWFR]/); //Matches first letter of boulder judgement (colour tag)
+        let judgement = BoulderJudgement.match(/[GLBWFR]#(.*)/); //Matched Everything after # (Boulder judgement)
+        return colour, judgement
+    }
 }
-callPackWithoutLackAPI('EH39JN', '2', '4')
