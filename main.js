3c58f90c662dfb9a5f801ce227bda17ad44ea306
Calum McM <calummcmeekin@me.com>
2019-06-27 07:24:10 +0100

    Bein creating server for python files

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
+import * as execSync from child_process;
+var output;
+
 class APICall{
-    constructor(location, days, hours, output){
+    constructor(location, days, hours){
         this._location = location;
         this._days = days;
         this._hours = hours;
-        this._output = output;
     }
     get location(){
         return this._location;
@@ -13,9 +15,6 @@ class APICall{
     }
     get hours(){
         return this._hours;
-    }
-    get output(){
-        return this._output;
     }
     set location(newLocation){
         this._location = newLocation;
@@ -30,14 +29,24 @@ class APICall{
         // Instantiate the Shell object and invoke its execute method.
         const execSync = require('child_process').execSync;
         call = 'python PackWithoutLack.py ' + this._location + ' ' +  this._days + ' ' + this._hours
-        const output = execSync(call, { encoding: 'utf-8' }); 
-        this._output = output; //Setter for output
+        output = execSync(call, { encoding: 'utf-8' }); 
+        console.log(output);
     }
     deconstructOutput(){
-        const BoulderJudgement = this._output['BoulderJudgement'];
+        const BoulderJudgement = output['BoulderJudgement'];
         //Colour Tags: G OKGREEN, L OKLIGHTBLUE, B BLUE, W WARNING, F FAIL, R REDUNDERBLACK
         let colour = BoulderJudgement.match(/[GLBWFR]/); //Matches first letter of boulder judgement (colour tag)
         let judgement = BoulderJudgement.match(/[GLBWFR]#(.*)/); //Matched Everything after # (Boulder judgement)
         return colour, judgement
     }
+}
+function callFunction(){
+    location = 'EH£9jN';
+    days = '2';
+    hours ='6';
+    // Instantiate the Shell object and invoke its execute method.
+    const execSync = require('child_process').execSync;
+    call = 'python PackWithoutLack.py ' + location + ' ' +  days + ' ' + hour;
+    output = execSync(call, { encoding: 'utf-8' }); 
+    document.write(console.log(output));
 }
