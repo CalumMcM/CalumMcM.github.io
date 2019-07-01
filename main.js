48a51289c9e55c9f6d108d41589ab40540fe610d
Calum McMeekin <calummcmeekin@me.com>
2019-07-01 15:37:13 +0100

    Now works for days >2

--- main.js
+++ main.js
@@ -2,42 +2,50 @@
 
 function callPackWithoutLack(locationGIVEN, daysGIVEN, hoursGIVEN){
     var xhttp = new XMLHttpRequest();
-    console.log(locationGIVEN + "HELLO");
+    if (hoursGIVEN == ""){
+        hoursGIVEN = 0;
+    }
     xhttp.open("GET", "http://thebravesttoaster.pythonanywhere.com/main/"+locationGIVEN+"/"+daysGIVEN+"/"+hoursGIVEN, true);
     console.log("Powered By Dark Sky");
     xhttp.send();
     xhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
         ClothesDict = (this.responseText);
-        console.log(ClothesDict);
         returnBoulderJudgement(ClothesDict);
         returnClothes('True', 1, 'InvisConLeft1');
         returnClothes('True', 0, 'InvisConRight1');
         returnClothes('False', 1, 'InvisConLeft2');
         returnClothes('False', 0, 'InvisConRight2');
-        returnSummary(ClothesDict);
+        returnSummary(ClothesDict, daysGIVEN);
       }
     };
 }
-function returnBoulderJudgement(ClothesDictUNJ){
+function returnBoulderJudgement(ClothesDictUNJ, daysGIVEN){
     ClothesDict = JSON.parse(ClothesDictUNJ)
-    console.log(ClothesDict);
     boulderJudgementFull = ClothesDict['BoulderJudgement'];
-    console.log(boulderJudgementFull);
-    //Colour Tags: G OKGREEN, L OKLIGHTBLUE, B BLUE, W WARNING, F FAIL, R REDUNDERBLACK
-    let colour = boulderJudgementFull.match(/Green|skyblue|Blue|Warning|89243A|Red/); //Matches first letter of boulder judgement (colour tag)
-    let judgement = boulderJudgementFull.match(/[^\|]*$/); //Matched Everything after # (Boulder judgement)
-    if (colour[0] == 'Warning'){
-        document.getElementById('BoulderResultsContainerRight').style.backgroundColor = "yellow";
-        document.getElementById('BoulderResultsContainerRight').innerHTML = "<span style='color:black'>"+judgement[0]+"</span>";
+    if (daysGIVEN < 2){
+        //Colour Tags: G OKGREEN, L OKLIGHTBLUE, B BLUE, W WARNING, F FAIL, R REDUNDERBLACK
+        let colour = boulderJudgementFull.match(/Green|skyblue|Blue|Warning|89243A|Red/); //Matches first letter of boulder judgement (colour tag)
+        let judgement = boulderJudgementFull.match(/[^\|]*$/); //Matched Everything after # (Boulder judgement)
+        if (colour[0] == 'Warning'){
+            document.getElementById('BoulderResultsContainerRight').style.backgroundColor = "yellow";
+            document.getElementById('BoulderResultsContainerRight').innerHTML = "<span style='color:black'>"+judgement[0]+"</span>";
+        } 
+        else if (colour[0] == 'Red'){
+            document.getElementById('BoulderResultsContainerRight').style.backgroundColor = "Red";
+            document.getElementById('BoulderResultsContainerRight').innerHTML = "<span style='color:white'>"+judgement[0]+"</span>";
+        } 
+        else {
+            document.getElementById('BoulderResultsContainerRight').innerHTML = "<span style='color:"+colour+"'>"+judgement[0]+"</span>";
+            console.log(judgement[0]);
+        }
     } 
-    else if (colour[0] == 'Red'){
-        document.getElementById('BoulderResultsContainerRight').style.backgroundColor = "Red";
-        document.getElementById('BoulderResultsContainerRight').innerHTML = "<span style='color:white'>"+judgement[0]+"</span>";
-    } else {
-        document.getElementById('BoulderResultsContainerRight').innerHTML = "<span style='color:"+colour+"'>"+judgement[0]+"</span>";
-        console.log(judgement[0]);
+    else {
+        document.getElementById('BoulderResultsContainerLeft').innerHTML = "<center> <h3> Boulderable Days </h3></center>";
+        //for (let boulderAbleDay in boulderJudgementFull)
+        document.getElementById('BoulderResultsContainerRight').innerHTML += "<span style='color:Green'>" + boulderJudgementFull + "</span>";
     }
+    
 }
 function returnClothes(Truth, divider, container){
     let iterator = 1;
